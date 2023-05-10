5744450faedb7e6c9356ca33102320264b51d21c
Dmitry <45977521+DmitryLebovski@users.noreply.github.com>
2023-05-10 11:07:39 +0300

    first init

---
+++ bsbo-011215-22/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using bsbo_011215_22;
+
+internal class Application
+{
+    // Сортировка Стэка на массивах
+    static void QueueSort()
+    {
+        int n = 5;
+
+        Queue queue = new Queue(); 
+        Random rnd = new Random();
+
+        for (int i = 0; i < n; i++)
+        {
+            queue.Enqueue(rnd.Next(0, 100));
+        }
+
+        queue.Print();
+        queue.Dequeue();
+        queue.Print();
+
+        /*
+        bool swapFlag = false;
+        for (int i = 0; i < n; i++)
+        {
+            swapFlag = false;
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (queue[j] > queue[j + 1])
+                {
+                    (queue[j], queue[j + 1]) = (queue[j + 1], queue[j]);
+                    swapFlag = true;
+                }
+            }
+
+            if (!swapFlag)
+                break;
+        }
+
+        queue.Print();
+        */
+
+    }
+
+    static void Main(string[] args)
+    {
+        QueueSort();
+    }
+}
+
---
+++ bsbo-011215-22/Queue.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bsbo_011215_22
+{
+    public class Queue
+    {
+        private List<int> _list;
+
+        public Queue() {
+            _list = new List<int>();
+        }
+
+        // Получение длины очереди
+        public int Count {
+            get { return _list.Count; }
+        }
+
+        // Проверка очереди на пустоту
+        public bool isEmpty()
+        {
+            return Count == 0;
+        }
+
+        // Добавление элемента в очередь
+        public void Enqueue(int item)
+        {
+            _list.Add(item);
+        }
+
+        // Удаление элемента из очереди
+        public int Dequeue()
+        {
+           if (isEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            int item = _list[0];
+            _list.RemoveAt(0);
+            return item;
+        }
+        
+        // Вывод элемента в начале очереди без его удаления
+        public int Peek() {
+            if (isEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            
+            return _list[0];
+        }
+
+        // Перегрузка оператора индексации [] 
+        public int this[int index]
+        {
+            get {
+                if (index < 0 || index >= Count) { 
+                    throw new ArgumentOutOfRangeException("index");
+                }
+                return _list[index % Count];
+            }
+
+            set {
+                if (index < 0 || index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException("index");
+                }
+                _list[index % Count] = value;
+            }
+        }
+
+
+        // Вывод списка в консоль
+        public void Print()
+        {
+            foreach (int item in _list)
+            {
+                Console.Write($"{item.ToString()} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
