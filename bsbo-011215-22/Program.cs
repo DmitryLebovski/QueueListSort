2e0541532ea978c4c6dc1588b109ebe5cb65aa57
Dmitry <45977521+DmitryLebovski@users.noreply.github.com>
2023-05-27 16:18:41 +0300

    F(n), O(f(n)) done
    пересчитать полином с внесенными изменениями

--- bsbo-011215-22/Program.cs
+++ bsbo-011215-22/Program.cs
@@ -4,117 +4,67 @@ using System.Diagnostics;
 internal class Application
 {
     static public long N_OP = 0;
-    static public int n = 10; // начальный размер сортируемой очереди
+    static public int n = 10; //размер сортируемой очереди
 
     static void Main(string[] args)
     {
         var x = new Stopwatch();
         Random rnd = new Random();
 
-        Queue queue = new Queue();
-        // Заполнение очереди n случайными элементами
-        for (int i = 0; i < 500; i++)
-        {
-            queue.Enqueue(rnd.Next(1000));
-        }
-        N_OP = 0;
-        x.Restart();
-
-        Console.WriteLine("До сортировки:");
-        queue.Print();
-
-        // Начало сортировки //
-        // Переменная-флаг, по которой отслеживается факт выполнения перестановки на итерации
-        N_OP += 1;
-        bool swapFlag = false; // 1
-
-        N_OP += 3;
-        //4
-        for (int i = 0; i < queue.Count; i++)
-        {
-            // сбрасывание флага перестановки
-            swapFlag = false; // 1
-            N_OP += 4;
-
-            N_OP += 5;
-            for (int j = 0; j < queue.Count - i - 1; j++) // 5
-            {
-                N_OP += 4;
-                if (queue[j] > queue[j + 1])
-                {
-                    (queue[j], queue[j + 1]) = (queue[j + 1], queue[j]); 
-                    N_OP += 9;
-                    swapFlag = true;
-                }
-                N_OP += 5;
-            }
-
-            // Если на итерации не было перестановок, значит очередь отсортирована, поэтому можно закончить работу цикла
-            N_OP += 2;
-            if (!swapFlag) // 2
-            {
-                N_OP += 1;
-                break; // 1
-            }
-        }
-        // Конец сортировки //
-        Console.WriteLine("После сортировки:");
-        queue.Print();
-
-        /*
-        for (int n = 1; n <= 10; n++) 
+        for (int n = 1; n <= 10; n++)
         {
             Queue queue = new Queue();
             // Заполнение очереди n случайными элементами
-            for (int i = 0; i < n * 100; i++) 
+            for (int i = 0; i < n * 100; i++)
             {
                 queue.Enqueue(rnd.Next(1000));
             }
             N_OP = 0;
             x.Restart();
-
+            ////////////////////////////////////////////////////////////////////////
+            //F(n) = 498n^3+70n^2+9n+4
+            //O(F(n)) = n^3
             // Начало сортировки //
             // Переменная-флаг, по которой отслеживается факт выполнения перестановки на итерации
-            N_OP += 1;
-            bool swapFlag = false; // 1
 
-            N_OP += 3;
-            //4
-            for (int i = 0; i < queue.Count; i++)
+            N_OP += 1;
+            bool swapFlag = false;                  // 1
+
+            Application.n += 3;
+            for (int i = 0; i < queue.Count; i++)			// 3 + ∑_(i=1)^n▒〖(1+498n^2+70n+5+3)=〗 498n^3+70n^2+9n+3
             {
                 // сбрасывание флага перестановки
-                swapFlag = false; // 1
-                N_OP += 4;
+                swapFlag = false;                   // 1
+                N_OP += 1;
 
                 N_OP += 5;
-                for (int j = 0; j < queue.Count - i - 1; j++) // 5
+                for (int j = 0; j < queue.Count - i - 1; j++)	// 5 + ∑_(i=1)^n▒〖(148n+25+350n+39+1+5)=〗 498n^2+70n+5  
                 {
+
                     N_OP += 4;
-                    if (queue[j] > queue[j + 1]) // 4, 9n + 6
+                    if (queue[j] > queue[j + 1]) // 7 + 9+74n + 9+74n = 148n+25
                     {
-                        (queue[j], queue[j + 1]) = (queue[j + 1], queue[j]); // 2 * (9n + 6) + 2 * (10n + 9)
-                        N_OP += 9;
-                        swapFlag = true;
+                        N_OP += 7;
+                        (queue[j], queue[j + 1]) = (queue[j + 1], queue[j]); // 7 + 2*(8+101n + 8+74n) = 39 + 350n
+
+                        N_OP += 1;
+                        swapFlag = true; 				// 1
                     }
-                    N_OP += 5;
+                    N_OP += 5; //5
                 }
 
                 // Если на итерации не было перестановок, значит очередь отсортирована, поэтому можно закончить работу цикла
-                N_OP += 2;
                 if (!swapFlag) // 2
                 {
-                    N_OP += 1;
-                    break; // 1
+                    N_OP += 2;
+                    break;
                 }
             }
             // Конец сортировки //
-
-
+            ////////////////////////////////////////////////////////////////////////
             x.Stop();
             string k = x.ElapsedMilliseconds.ToString();
             Console.WriteLine($"Номер сортировки: {n} Количество отсортированных элементов: {n * 100} Время сортировки (ms): {k} Количество операций (N_Op): {N_OP}");
         }
-        */
     }
 }
-
--- bsbo-011215-22/Queue.cs
+++ bsbo-011215-22/Queue.cs
@@ -4,139 +4,130 @@
     {
         public int Capacity;
         int[] _list; // Массив чисел
-        public int Count = 0; // Размер текущей очереди (1)
+        public int Count = 0; // Размер текущей очереди		// 1
 
-        public Queue()
+        public Queue()							// (1+2+4)=7
         {
-            Capacity = Application.n; // 2
-            _list = new int[Application.n]; // 4
+            Capacity = Application.n; 						// 2
+            _list = new int[Application.n]; 				// 4
             Application.N_OP += 7;
         }
 
         // Проверка на пустоту очереди
-        public bool isEmpty()
+        public bool isEmpty()						// 1
         {
             Application.N_OP += 1;
-            return Count == 0; // 1
+            return Count == 0; 						// 1
         }
 
         // Добавление элемента в очередь
-        public void Enqueue(int item)
+        public void Enqueue(int item)					// 1+(18+2)+3+2=26
         {
-            Application.N_OP += 1;
             // Если массив заполнен, а нужно добавить еще элементы - размер массива "увеличивается" 
-            if (Count == Capacity) // 1
+            if (Count == Capacity)					// 1
             {
-                Array.Resize(ref _list, Capacity * 2); // 3 + 13 = 16 
-                Capacity *= 2; // 2
-                Application.N_OP += 18;
+                Array.Resize(ref _list, Capacity * 2);		// 15 + 3 = 18
+                Capacity *= 2;						// 2
             }
-            _list[Count] = item; // 3
-            Count++; // 1
-            Application.N_OP += 4;
+            _list[Count] = item;					// 3
+            Count++; 							// 2
+            Application.N_OP += 26;
         }
 
         // Удаление элемента из очереди
-        public int Dequeue()
+        public int Dequeue()						// 39
         {
-            if (isEmpty()) { 
+            if (isEmpty())
+            {						// 1
                 throw new InvalidOperationException("Cannot index: Queue is empty.");
-            }// 1
-            Application.N_OP += 1;
+            }
 
-
-            Application.N_OP += 38;
-            int res = _list[0]; // 3
-            Array.Copy(_list, 1, _list, 0, Capacity - 1); // 6 + 
-            Count--; // 1
-            return res; // 1
+            int res = _list[0];						// 3
+            Array.Copy(_list, 1, _list, 0, Capacity - 1); 	// 6 + 27 = 33
+            Count--;							// 2
+            Application.N_OP += 39;
+            return res;
         }
 
         // Получение значения элемента очереди
-        public int Get(int index)
+        public int Get(int index)					// 6+74n
         {
-            Application.N_OP += 3;
-            if (index < 0 || index >= Count) {
+            if (index < 0 || index >= Count) // 3
+            {
                 throw new IndexOutOfRangeException();
             }
+            Application.N_OP += 3;
 
             // Переменная для сохранения нужного элемента
-            int item = 0; Application.N_OP += 1;
+            int item = 0;							// 1
+            Application.N_OP += 1;
 
-            Application.N_OP += 1; 
+            Application.N_OP += 2;
             // Извлекаем элементы из очереди и восстанавливаем после получения нужного элемента
-            for (int i = 0; i < Count; i++)
+            for (int i = 0; i < Count; i++)				//2+∑_(i=1)^n▒〖(42+1+1+28+2)=〗 2+74n
             {
+                int current = Dequeue(); 				// 2+39=41
                 Application.N_OP += 2;
-                int current = Dequeue();// 2
 
                 Application.N_OP += 1;
-                //1
-                if (i == index)
+                if (i == index)						// 1
                 {
                     // Сохраняем нужный элемент
-                    item = current; // 1
+                    item = current;					// 1
                     Application.N_OP += 1;
                 }
-
+                Enqueue(current);					// 2+26=28 
                 Application.N_OP += 2;
-                Enqueue(current); // 2
 
                 Application.N_OP += 2;
             }
             return item;
         }
-
         // Установка значения элемента очереди по индексу
         // Та же логика, что и в Get(), но вместо возврата значения элемента ему устанавливается новое значение.
-        public void Set(int index, int value) 
+        public void Set(int index, int value)			// 5+101n
         {
-            //3
             Application.N_OP += 3;
-            if (index < 0 || index >= Count)
+            if (index < 0 || index >= Count)				// 3
                 throw new IndexOutOfRangeException();
 
-            Application.N_OP += 1;
+            Application.N_OP += 2;
             // Извлекаем элементы из очереди и восстанавливаем после замены нужного элемента
-            for (int i = 0; i < Count; i++)
+            for (int i = 0; i < Count; i++) 				// 2+∑_(i=1)^n▒〖(42+1+28+28+2)=〗 2+101n
             {
                 Application.N_OP += 2;
-                int current = Dequeue(); // 2
+                int current = Dequeue(); 				// 2+39=41
 
                 Application.N_OP += 1;
-                if (i == index)
+                if (i == index)						// 1
                 {
                     // Заменяем элемент на нужном индексе
-                    Enqueue(value); // 2
+                    Enqueue(value);                     // 2+26=28
                     Application.N_OP += 2;
                 }
                 else
                 {
                     // Восстанавливаем остальные элементы
-                    Enqueue(current); // 2
+                    Enqueue(current);                   // 2+26=28
                     Application.N_OP += 2;
                 }
-
                 Application.N_OP += 2;
             }
         }
-
         // Перегрузка []
         public int this[int index]
         {
-            get // 2
+            get
             {
                 Application.N_OP += 2;
-                return Get(index); 
+                return Get(index); 					// 2+(6+74n)=8+74n
             }
-
-            set // 3
+            set
             {
                 Application.N_OP += 3;
-                Set(index, value); 
+                Set(index, value); 					// 3+(5+101n)=8+101n
             }
         }
-
         // Вывод очереди в консоль
         public void Print()
         {
@@ -150,4 +141,3 @@
         }
     }
 }
-
